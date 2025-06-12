b61d9fcc48957fff440264aed7608bab3dd49cc6
wallisonc2k <37313446+wallisonc2k@users.noreply.github.com>
2025-06-12 12:50:59 -0300

    Calendário de eventos (#20)

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ListaNoticiasView, DetalheNoticiaView
+from .views import ListaNoticiasView, DetalheNoticiaView, CalendarioView, calendario_api
 
 app_name = 'noticias'
 
 urlpatterns = [
+    # URLs do calendário
     path('', ListaNoticiasView.as_view(), name='lista'),
+    
+    path('calendario/', CalendarioView.as_view(), name='calendario'),
+    path('api/calendario/', calendario_api, name='calendario_api'),
+ 
     path('<slug:slug>/', DetalheNoticiaView.as_view(), name='detalhe'),
 ]
