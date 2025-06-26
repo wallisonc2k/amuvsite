558ba70f4aa806f590c886cad9ec593254201faf
wallisonc2k <37313446+wallisonc2k@users.noreply.github.com>
2025-06-26 18:38:32 -0300

    Dev (#30)
    * + lib requeriments.txt
    
    * Primeira versão de imagens
    
    * Hero slider com fade
    
    * Removi banner1 de teste
    
    * Opção de patrocinadores add
    
    * Opção de alterar qr code pix
    
    * Removi o hero slide da views, pois agora uso templatetags assim como para os patrocinadores
    
    * Envio do user no context da templatetag

---
+++ core/templatetags/media_tags.py
@@ -0,0 +1,57 @@
+# media_site/templatetags/media_tags.py
+
+from django import template
+from django.db.models import Q
+from ..models import ImagemSite
+
+register = template.Library()
+
+@register.inclusion_tag('partials/hero_slider.html', takes_context=True)
+def hero_slider(context):
+    """Renderiza o hero slider da home"""
+    imagens = ImagemSite.objects.por_tipo('hero_slider')
+    return {'imagens': imagens, 'user': context['user']}
+
+@register.simple_tag
+def hero_slider_json():
+    """Retorna as URLs das imagens do hero slider em formato JSON para Alpine.js"""
+    import json
+    imagens = ImagemSite.objects.por_tipo('hero_slider')
+    urls = [imagem.imagem.url for imagem in imagens] if imagens.exists() else []
+    return json.dumps(urls)
+
+@register.inclusion_tag('partials/galeria_home.html')
+def galeria_home():
+    """Renderiza a galeria da home"""
+    imagens = ImagemSite.objects.por_tipo('galeria')
+    return {'imagens': imagens}
+
+@register.simple_tag
+def get_imagem_site(tipo):
+    """Retorna uma imagem específica por tipo"""
+    try:
+        return ImagemSite.objects.por_tipo(tipo).first()
+    except ImagemSite.DoesNotExist:
+        return None
+
+@register.simple_tag
+def get_imagens_site(tipo):
+    """Retorna múltiplas imagens por tipo"""
+    return ImagemSite.objects.por_tipo(tipo)
+
+@register.simple_tag
+def logo_pequena():
+    """Retorna a logo pequena"""
+    return ImagemSite.objects.por_tipo('logo_pequena').first()
+
+@register.simple_tag
+def logo_grande():
+    """Retorna a logo grande"""
+    return ImagemSite.objects.por_tipo('logo_grande').first()
+
+@register.filter
+def get_alt_text(imagem):
+    """Filtro para obter o alt text apropriado"""
+    if imagem:
+        return imagem.get_alt_text()
+    return ""
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.shortcuts import render
-from .models import Contato
+from .models import Contato, ImagemSite
 from .forms import ContatoForm
 from .utils import listar_imagens_estaticas, listar_imagens_com_descricao
 from noticias.models import Noticia
@@ -14,13 +14,9 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        imagens_hero_slider = listar_imagens_estaticas('shared/img/hero_slider') # Adiciona as imagens do hero Slider automaticamente
         imagens_swiper = listar_imagens_com_descricao()
-        imagens_patrocinadores = listar_imagens_estaticas('shared/img/patrocinadores/miniatura')[:5] # Adiciona as imagens dos patrocinadores
-        
-        context['imagens_hero_slider'] = imagens_hero_slider
+
         context['imagens_swiper'] = imagens_swiper
-        context['imagens_patrocinadores'] = imagens_patrocinadores
         context['noticias'] = Noticia.objects.filter(categoria='noticia').order_by('-publicado_em')[:2]  # últimas 3 notícias
         context['eventos'] = Noticia.objects.filter(categoria='evento',
                                                     data_evento__gte=timezone.now()
